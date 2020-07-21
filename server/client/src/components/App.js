5af6059a70732483804377434b03cfe825957837
Emilio Encarnacion <emilio.encarnacion@tufts.edu>
2020-07-21 21:58:00 +0800

    adding files

---
+++ server/client/src/components/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import Header from './Header';
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
+
+
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        //Check if user is authenticated:
+        const authenticatedUser = this.props.auth;
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        {authenticatedUser ? (
+                            <div>
+                                <Redirect to="/surveys" />
+                                <Route exact path="/surveys" component={Dashboard} />
+                                <Route path="/surveys/new" component={SurveyNew} />
+                            </div>
+                        ) : (
+                            <div>
+                                <Route exact path="/" component={Landing} />
+                            </div>
+                        )}
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps, actions)(App);
